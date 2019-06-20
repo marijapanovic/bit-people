5c6101f756130f214331c20a5adba5978f6881de
mprokic <marijapanovic1@gmail.com>
2019-06-20 12:31:16 +0200

    Updates

---
+++ precache-manifest.c10fe3bea459739289f4ac384fcd9361.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39a4f35549d065483004",
+    "url": "/bit-people/static/js/runtime~main.007689ab.js"
+  },
+  {
+    "revision": "bc135548c1380eae8aec",
+    "url": "/bit-people/static/js/main.132bd149.chunk.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/bit-people/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "bc135548c1380eae8aec",
+    "url": "/bit-people/static/css/main.84d3cc13.chunk.css"
+  },
+  {
+    "revision": "42a78f2a5854495233de9c4314f545e2",
+    "url": "/bit-people/index.html"
+  }
+];
